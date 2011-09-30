d1fc8d9eb51d89019256b3b4b96cf89891d30149
Tom Possin <tompossin@gmail.com>
2011-09-30 17:04:25 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studyworx3::Application.config.secret_token = 'a877a229333d3c606167837c1439951fa7085910bca20a64183effe60324333a7bf27a30ddcd9273132fbf90e98b8429065f13cdafc9f186caf9fca88e64da61'
