5bcf6b70bb27ca6ac3ee57f9078b21f02c523703
Tom Possin <tompossin@gmail.com>
2013-05-18 18:24:52 -0600

    Fixed multi-part emailing

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,4 +1,6 @@
 class MessageMailer < ActionMailer::Base
+  include FormatContent
+  
   default from: "admin@studyworx.net"
   
   # This sends messages to individule users when private messaging
@@ -7,6 +9,7 @@ class MessageMailer < ActionMailer::Base
     @sender = sender
     @recipient = recipient
     @message = message
+    @html_body = markdown_to_html(message.body)
     to_addr = "#{recipient.fullname} <#{recipient.email}>"
     from_addr = "#{sender.fullname} <#{sender.email}>"
     @url = "http://studyworx.net/schools"
@@ -16,12 +19,13 @@ class MessageMailer < ActionMailer::Base
       subject: email_subject )
   end
   
-  # This sends messages to all team members. 
+  # This sends messages to all team members.
   #  Called from team_messages_controller
   def team_email(sender,team,email_subject,message)
     @sender = sender
     recipient = team.users.all.map(&:email)
     @message = message
+    @html_body = markdown_to_html(message.body)
     to_addr = recipient
     from_addr = "#{sender.fullname} <#{sender.email}>"
     @url = "http://studyworx.net/schools"
