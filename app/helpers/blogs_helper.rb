397ceb4c54495ef0e4a75313b957507d006f93da
Tom Possin <tompossin@gmail.com>
2013-06-05 13:38:33 -0600

    Fixed display of Blogs index

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,14 @@
 module BlogsHelper
+  
+  # This changes the status display depending on the value of blog.leadstory
+  def story_status(leadstory)
+    if leadstory == nil
+      return "Draft"
+    elsif leadstory == true
+      return "True"
+    elsif leadstory == false
+      return "False"
+    end
+  end
+  
 end
