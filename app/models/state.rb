2f2f7e2449950b4296e4c45a63d6ee24c00515b9
Tom Possin <tompossin@gmail.com>
2013-09-16 15:48:25 -0600

    Fixed nil error in state.rb

--- app/models/state.rb
+++ app/models/state.rb
@@ -19,10 +19,15 @@ class State < ActiveRecord::Base
   # This updates the state to "uptodate" after charts are rebuilt.
   def self.reset_state(user_id,task_id)
     state = self.where("user_id = ? and task_id = ?",user_id,task_id).first
-    unless state.uptodate
-      state.uptodate = true
-      state.save
+    unless state
+      state = State.create(user_id: user_id, task_id: task_id, uptodate: true)
+    else
+      unless state.uptodate
+        state.uptodate = true
+        state.save
+      end
     end
+    return state
   end
   
 end
